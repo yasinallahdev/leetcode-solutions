d55674ba86221364a29d8f12845903624aef2de3
Ya Sin Supreme Allah <yasin.allahdev@gmail.com>
2021-04-17 23:11:28 -0400

    Two Sum (Brute Force Version)

---
+++ C++ Leetcodes/twosum-bruteforce.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int x = 0; x < nums.size(); x++) {
+            for(int y = 0; y < nums.size(); y++) {
+                if(x == y) {
+                    continue;
+                } else {
+                    if(nums.at(x) + nums.at(y) == target) {
+                        return {x, y};
+                    }
+                }
+            }
+        }
+        return {0, 1};
+    }
+};
